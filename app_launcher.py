4be2688f563fe8091076fb970c6875febf4eac8e
Navanit Sharma <navanit.231519@ncit.edu.np>
2025-12-30 20:59:13 +0545

    Update app_launcher.py

--- app_launcher.py
+++ app_launcher.py
@@ -2,5 +2,6 @@ import os
 
 class AppLauncher:
     def open_application(self, app_name):
-        os.system(f'start {app_name}')  # For Windows
+        os.system(f'start {app_name}')  
+
         
